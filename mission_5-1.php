6008689121ff5cd69da9f7318f9f79a22fbd382e
syma411 <46731769+syma411@users.noreply.github.com>
2019-09-13 19:41:04 +0900

    Update mission_5-1.php

--- mission_5-1.php
+++ mission_5-1.php
@@ -1,9 +1,9 @@
 	<?php
 	session_start();
 
-		$dsn = 'mysql:dbname=tb210303db;host=localhost';
-		$user = 'tb-210303';
-		$password = 'XNcnWp2zZV';
+		$dsn = 'mysql:dbname=*********;host=localhost';
+		$user = '*******';
+		$password = '*******';
 		$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 		global $pdo;
 		
